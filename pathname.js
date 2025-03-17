1d3994d3b47639549d80fbdfdc821135a68d0071
Irfan Amin <80665590+irfanamin24@users.noreply.github.com>
2025-03-17 15:10:46 +0700

    Update pathname.js

--- pathname.js
+++ pathname.js
@@ -2,3 +2,8 @@
             let newUrl = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search;
             window.history.replaceState(null, "", newUrl);
         }
+        if (window.location.pathname.endsWith("/index.html") && window.location.pathname !== "/index.html") {
+            let newUrl = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search;
+            window.history.replaceState(null, "", newUrl);
+        }
+
