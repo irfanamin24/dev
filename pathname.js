ac2cd25d33320a586ffc2cc12cb87d4c4a998573
Irfan Amin <80665590+irfanamin24@users.noreply.github.com>
2025-03-17 15:15:20 +0700

    Update pathname.js

--- pathname.js
+++ pathname.js
@@ -1,9 +1,5 @@
-        if (window.location.pathname.endsWith("/") && window.location.pathname !== "/") {
-            let newUrl = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search;
-            window.history.replaceState(null, "", newUrl);
-        }
-        if (window.location.pathname.endsWith("/index.html") && window.location.pathname !== "/index.html") {
-            let newUrl = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search;
-            window.history.replaceState(null, "", newUrl);
-        }
+if (window.location.pathname.endsWith("/") && window.location.pathname !== "/") {
+        let newUrl = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search;
+        window.history.replaceState(null, "", newUrl);
+}
 
